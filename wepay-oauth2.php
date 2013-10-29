891506e45f4fe28a6dd87a244072de32754236a9
Spencer Finnell <spencerfinnell@gmail.com>
2013-10-29 12:40:39 -0400

    Simplify cancelling as well. See previous commit.

--- wepay-oauth2.php
+++ wepay-oauth2.php
@@ -476,11 +476,8 @@ function awpo2_gateway_wepay_edd_wepay_get_api_creds( $creds, $payment_id ) {
 	/**
 	 * No cart items, check session
 	 */
-	if ( isset( $_GET[ 'edd-action' ] ) && 'charge_wepay_preapproval' == $_GET[ 'edd-action' ] && isset ( $_GET[ 'payment_id' ] ) ) {
+	if ( isset( $_GET[ 'edd-action' ] ) && in_array( $_GET[ 'edd-action' ], array( 'charge_wepay_preapproval', 'cancel_wepay_preapproval' ) ) && isset ( $_GET[ 'payment_id' ] ) ) {
 		$cart_items = edd_get_payment_meta_cart_details( $_GET[ 'payment_id' ] );
-	} else if ( isset( $_GET[ 'edd-action' ] ) && 'cancel_wepay_preapproval' == $_GET[ 'edd-action' ] && isset ( $_GET[ 'payment_id' ] ) ) {
-		$meta = edd_get_payment_meta( $_GET[ 'payment_id' ] );
-		$cart_items = maybe_unserialize( $meta[ 'downloads' ] );
 	} else if ( $payment_id ) {
 		$meta = edd_get_payment_meta( $payment_id );
 		$cart_items = maybe_unserialize( $meta[ 'downloads' ] );
